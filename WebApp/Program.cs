ab49bb61a37322facb60134f7e14dc90590824d4
Mikael Stoor <mikael.stoor@wapice.com>
2024-03-16 11:41:03 +0200

    addmasstransit

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,6 +1,51 @@
+using MassTransit;
+using Quartz;
+
+const string SchedulerQueueName = "repro-quartz-scheduler"; 
+var SchedulerQueue = new Uri("queue:" + SchedulerQueueName);
+var connectionString = "Server=(localdb)\\mssqllocaldb;Database=Repro;Trusted_Connection=True;";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddQuartz(q =>
+{
+    q.UseMicrosoftDependencyInjectionJobFactory();
+    q.UsePersistentStore(s =>
+    {
+        s.UseProperties = true;
+
+        s.UseSqlServer(connectionString);
+
+        s.UseJsonSerializer();
+    });
+});
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddPublishMessageScheduler();
+
+    x.AddMessageScheduler(SchedulerQueue);
+    x.AddQuartzConsumers(options => options.QueueName = SchedulerQueueName);
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+        cfg.UseMessageScheduler(SchedulerQueue);
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+builder.Services.AddQuartzHostedService(configurator =>
+{
+    //configurator.StartDelay = TimeSpan.FromSeconds(5);
+    configurator.WaitForJobsToComplete = true;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
